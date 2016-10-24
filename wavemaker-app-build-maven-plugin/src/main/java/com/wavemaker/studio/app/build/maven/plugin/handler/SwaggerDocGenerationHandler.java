13ea07d5f458947eb737b7bda3813916c435f023
sunilkumar <sunil.pulugula@wavemaker.com>
2016-10-24 20:01:33 +0530

    Generating swagger for feed and security service in mvn build time.
    Change-Id: Id30f356de9849a510b3e09c82bc980934c5a3f6d

--- wavemaker-app-build-maven-plugin/src/main/java/com/wavemaker/studio/app/build/maven/plugin/handler/SwaggerDocGenerationHandler.java
+++ wavemaker-app-build-maven-plugin/src/main/java/com/wavemaker/studio/app/build/maven/plugin/handler/SwaggerDocGenerationHandler.java
@@ -16,14 +16,21 @@
 package com.wavemaker.studio.app.build.maven.plugin.handler;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -40,22 +47,25 @@ import com.wavemaker.tools.apidocs.tools.core.model.Swagger;
  * Created by saddhamp on 20/4/16.
  */
 public class SwaggerDocGenerationHandler implements AppBuildHandler {
+    public static final String SERVICE_DEF_XML = "servicedef.xml";
+    public static final String SECURITY_SERVICE_TYPE = "SecurityServiceType";
+    public static final String FEED_SERVICE_TYPE = "FeedService";
+    public static final String SECURITY_SERVICE_CONTROLLER_CLAZZ = "com.wavemaker.runtime.security.controller.SecurityController";
+    public static final String FEED_SERVICE_CONTROLLER_CLAZZ = "com.wavemaker.runtime.feed.controller.FeedServiceController";
     private static final Logger logger = LoggerFactory.getLogger(SwaggerDocGenerationHandler.class);
-
     private static final String DESIGN_TIME_FOLDER = "designtime";
     private static final String SRC_FOLDER = "src";
     private static final String API_EXTENSION = "_API.json";
-
     private ObjectMapper objectMapper;
     private Folder servicesFolder;
     private URLClassLoader urlClassLoader;
-    private URL [] classPathURLs;
+    private URL[] classPathURLs;
 
-    public SwaggerDocGenerationHandler(Folder servicesFolder, URL [] classPathURLs) {
-        if(servicesFolder == null || !servicesFolder.exists()) {
+    public SwaggerDocGenerationHandler(Folder servicesFolder, URL[] classPathURLs) {
+        if (servicesFolder == null || !servicesFolder.exists()) {
             throw new WMRuntimeException("Services folder is null or does not exist");
         }
-        if(classPathURLs == null || classPathURLs.length == 0) {
+        if (classPathURLs == null || classPathURLs.length == 0) {
             throw new WMRuntimeException("No class path url provided");
         }
 
@@ -71,8 +81,8 @@ public class SwaggerDocGenerationHandler implements AppBuildHandler {
             urlClassLoader = new URLClassLoader(classPathURLs, Thread.currentThread().getContextClassLoader());
 
             List<Folder> serviceFolders = servicesFolder.list().folders().fetchAll();
-            if(serviceFolders.size() > 0){
-                for(Folder serviceFolder : serviceFolders){
+            if (serviceFolders.size() > 0) {
+                for (Folder serviceFolder : serviceFolders) {
                     generateSwaggerDoc(serviceFolder);
                 }
             }
@@ -89,8 +99,8 @@ public class SwaggerDocGenerationHandler implements AppBuildHandler {
 
     protected void generateSwaggerDoc(Folder serviceFolder) {
 
-        if(serviceFolder.exists()) {
-            String basePackage = new BasePackage(serviceFolder.getFolder(SRC_FOLDER)).getBasePackageName();
+        if (serviceFolder.exists()) {
+            String basePackage = getBasePackageName(serviceFolder);
 
             if (StringUtils.isNotBlank(basePackage)) {
                 SwaggerGenerator swaggerGenerator = new SwaggerGenerator(basePackage);
@@ -100,6 +110,38 @@ public class SwaggerDocGenerationHandler implements AppBuildHandler {
                 marshallAndWriteToFile(swagger, serviceFolder.getFolder(DESIGN_TIME_FOLDER));
             }
         }
+    }
+
+    private String getBasePackageName(final Folder serviceFolder) {
+        final File serviceDefXML = serviceFolder.getFolder(DESIGN_TIME_FOLDER).getFile(SERVICE_DEF_XML);
+        String serviceType = findServiceType(serviceDefXML);
+        if (serviceType != null && serviceType.equals(SECURITY_SERVICE_TYPE)) {
+            return SECURITY_SERVICE_CONTROLLER_CLAZZ;
+        } else if (serviceType != null && serviceType.equals(FEED_SERVICE_TYPE)) {
+            return FEED_SERVICE_CONTROLLER_CLAZZ;
+        }
+        final BasePackage basePackage = new BasePackage(serviceFolder.getFolder(SRC_FOLDER));
+        return basePackage.getBasePackageName();
+    }
+
+    private String findServiceType(final File serviceDefXML) {
+        String serviceType;
+        DocumentBuilderFactory dbFactory
+                = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = null;
+        InputStream is = null;
+        try {
+            dBuilder = dbFactory.newDocumentBuilder();
+            is = serviceDefXML.getContent().asInputStream();
+            Document doc = dBuilder.parse(is);
+            doc.getDocumentElement().normalize();
+            serviceType = doc.getDocumentElement().getAttribute("type");
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            throw new WMRuntimeException("failed to find serviceType from servicedef xml file", e);
+        } finally {
+            org.apache.commons.io.IOUtils.closeQuietly(is);
+        }
+        return serviceType;
     }
 
     protected void marshallAndWriteToFile(Swagger swagger, Folder designTimeFolder) {
