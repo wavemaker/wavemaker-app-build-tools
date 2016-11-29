16b2bb92ce005f4a42188168378884316d6326db
Arjun Sahasranam <arjun.sahasranam@wavemaker.com>
2016-11-29 13:45:52 +0530

    SAML IMPL-EXCLUDE JKS FILE FROM FILTERING
    Change-Id: I9ab0426c48b9f341e1800e7fa41de0e4388ea471

--- wavemaker-app-build-maven-plugin/src/main/java/com/wavemaker/studio/app/build/maven/plugin/mojo/AppBuildMojo.java
+++ wavemaker-app-build-maven-plugin/src/main/java/com/wavemaker/studio/app/build/maven/plugin/mojo/AppBuildMojo.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,14 +22,20 @@ import java.util.List;
 
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.Build;
+import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.*;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.shared.filtering.MavenFilteringException;
 import org.apache.maven.shared.filtering.MavenResourcesExecution;
 import org.apache.maven.shared.filtering.MavenResourcesFiltering;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
 
 import com.wavemaker.studio.app.build.maven.plugin.handler.AppBuildHandler;
 import com.wavemaker.studio.app.build.maven.plugin.handler.PageMinFileGenerationHandler;
@@ -46,14 +52,17 @@ import com.wavemaker.studio.common.io.local.LocalFolder;
 public class AppBuildMojo extends AbstractMojo {
 
     public static final String ENCODING = "UTF-8";
+    public static final String MAVEN_RESOURCES_PLUGIN = "maven-resources-plugin";
+    private static final String NON_FILTERED_FILE_EXTENSIONS = "nonFilteredFileExtensions";
 
     @Parameter(property = "project", required = true, readonly = true)
     private MavenProject project;
 
     @Parameter(property = "basedir", required = true, readonly = true)
-    private String baseDirectory;;
+    private String baseDirectory;
+    ;
 
-    @Parameter(name="pages-directory", defaultValue = "src/main/webapp/pages/")
+    @Parameter(name = "pages-directory", defaultValue = "src/main/webapp/pages/")
     private String pagesDirectory;
 
     @Parameter(name = "services-directory", defaultValue = "services")
@@ -74,24 +83,27 @@ public class AppBuildMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException, MojoFailureException {
         initializeHandlers();
 
-        for(AppBuildHandler appBuildHandler : appBuildHandlers){
+        for (AppBuildHandler appBuildHandler : appBuildHandlers) {
             appBuildHandler.handle();
         }
 
         final Build build = project.getBuild();
+        final List<String> nonFilteredFileExtensions = getNonFilteredFileExtensions(build.getPlugins());
+
+
         MavenResourcesExecution mavenResourcesExecution =
-                new MavenResourcesExecution ( build.getResources(), new File(outputDirectory), project,
-                        ENCODING, build.getFilters(),null, session);
+                new MavenResourcesExecution(build.getResources(), new File(outputDirectory), project,
+                        ENCODING, build.getFilters(), nonFilteredFileExtensions, session);
 
         try {
-            mavenResourcesFiltering.filterResources( mavenResourcesExecution );
+            mavenResourcesFiltering.filterResources(mavenResourcesExecution);
         } catch (MavenFilteringException e) {
             throw new WMRuntimeException("Failed to execute resource filtering ", e);
         }
     }
 
     private void initializeHandlers() throws MojoFailureException {
-        if(appBuildHandlers == null) {
+        if (appBuildHandlers == null) {
             appBuildHandlers = new ArrayList<AppBuildHandler>();
             Folder rootFolder = new LocalFolder(baseDirectory);
 
@@ -103,14 +115,14 @@ public class AppBuildMojo extends AbstractMojo {
 
             Folder servicesFolder = rootFolder.getFolder(servicesDirectory);
             if (servicesFolder.exists()) {
-                URL [] runtimeClasspathElements = getRuntimeClasspathElements();
+                URL[] runtimeClasspathElements = getRuntimeClasspathElements();
                 appBuildHandlers.add(new SwaggerDocGenerationHandler(servicesFolder, runtimeClasspathElements));
                 appBuildHandlers.add(new VariableServiceDefGenerationHandler(rootFolder));
             }
         }
     }
 
-    private URL [] getRuntimeClasspathElements() throws MojoFailureException{
+    private URL[] getRuntimeClasspathElements() throws MojoFailureException {
         URL[] runtimeUrls = null;
         try {
             List<String> runtimeClasspathElements = project.getRuntimeClasspathElements();
@@ -120,11 +132,30 @@ public class AppBuildMojo extends AbstractMojo {
                 runtimeClasspathElement = runtimeClasspathElements.get(i);
                 runtimeUrls[i] = new File(runtimeClasspathElement).toURI().toURL();
             }
-        } catch (Exception exception){
+        } catch (Exception exception) {
             throw new MojoFailureException("Failed resolve project dependencies", exception);
         }
 
         return runtimeUrls;
+    }
+
+    private List<String> getNonFilteredFileExtensions(List<Plugin> plugins) {
+        for (Plugin plugin : plugins) {
+            if (MAVEN_RESOURCES_PLUGIN.equals(plugin.getArtifactId())) {
+                final Object configuration = plugin.getConfiguration();
+                if (configuration != null) {
+                    Xpp3Dom xpp3Dom = (Xpp3Dom) configuration;
+                    final Xpp3Dom nonFilteredFileExtensions = xpp3Dom.getChild(NON_FILTERED_FILE_EXTENSIONS);
+                    List<String> nonFilteredFileExtensionsList = new ArrayList<>();
+                    final Xpp3Dom[] children = nonFilteredFileExtensions.getChildren();
+                    for (Xpp3Dom child : children) {
+                        nonFilteredFileExtensionsList.add(child.getValue());
+                    }
+                    return nonFilteredFileExtensionsList;
+                }
+            }
+        }
+        return null;
     }
 }
 
