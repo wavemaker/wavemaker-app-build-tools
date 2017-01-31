dd7b656113fb596cdd8429960b9847fe832abfb7
Deepika Srimanthula <deepika.srimanthula@wavemaker.com>
2017-01-31 11:37:37 +0530

    WaveMaker-0# Updated CopyRight info
    Change-Id: Iabdeac1e109b7f7b94992b12a40d68b6e2123018

--- wavemaker-app-build-utils/src/main/java/com/wavemaker/studio/app/build/pages/PageMinFileGenerator.java
+++ wavemaker-app-build-utils/src/main/java/com/wavemaker/studio/app/build/pages/PageMinFileGenerator.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2015 WaveMaker, Inc.
+ * Copyright © 2013 - 2017 WaveMaker, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
