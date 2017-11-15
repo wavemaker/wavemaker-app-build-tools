788cadf0735569787164ae7ec6d048a7e31b2e5b
Srujan <srujan.thammisetty@wavemaker.com>
2017-11-15 21:31:16 +0530

    XMLUtils : Refactored code to use XMLUtils class to parse xml file.
    Change-Id: I105b171836e9460b5c3b29f075859268c57fcd15

--- wavemaker-app-build-maven-plugin/src/main/java/com/wavemaker/app/build/maven/plugin/handler/SwaggerDocGenerationHandler.java
+++ wavemaker-app-build-maven-plugin/src/main/java/com/wavemaker/app/build/maven/plugin/handler/SwaggerDocGenerationHandler.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,22 +16,15 @@
 package com.wavemaker.app.build.maven.plugin.handler;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.List;
 import java.util.Objects;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
--- wavemaker-app-build-utils/src/main/java/com/wavemaker/app/build/ProjectServicesHelper.java
+++ wavemaker-app-build-utils/src/main/java/com/wavemaker/app/build/ProjectServicesHelper.java
@@ -1,18 +1,10 @@
 package com.wavemaker.app.build;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
-import com.wavemaker.commons.WMRuntimeException;
 import com.wavemaker.commons.io.File;
 import com.wavemaker.commons.io.Folder;
+import com.wavemaker.commons.util.XMLUtils;
 
 /**
  * Created by srujant on 23/8/17.
@@ -22,27 +14,15 @@ public class ProjectServicesHelper {
     private static final String DESIGN_TIME_FOLDER = "designtime";
     private static final String SERVICE_DEF_XML = "servicedef.xml";
 
-    public static String findServiceType(Folder serviceFolder){
+    public static String findServiceType(Folder serviceFolder) {
         final File serviceDefXML = serviceFolder.getFolder(DESIGN_TIME_FOLDER).getFile(SERVICE_DEF_XML);
         if (!serviceDefXML.exists()) {
             return null;
         }
         String serviceType;
-        DocumentBuilderFactory dbFactory
-                = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = null;
-        InputStream is = null;
-        try {
-            dBuilder = dbFactory.newDocumentBuilder();
-            is = serviceDefXML.getContent().asInputStream();
-            Document doc = dBuilder.parse(is);
-            doc.getDocumentElement().normalize();
-            serviceType = doc.getDocumentElement().getAttribute("type");
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            throw new WMRuntimeException("failed to find serviceType from servicedef xml file", e);
-        } finally {
-            org.apache.commons.io.IOUtils.closeQuietly(is);
-        }
+        Document document = XMLUtils.getDocument(serviceDefXML);
+        document.getDocumentElement().normalize();
+        serviceType = document.getDocumentElement().getAttribute("type");
         return serviceType;
 
     }
